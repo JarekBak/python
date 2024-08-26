eaf6951fbebb8270ba522ba77c69ce448d1f166b
Jarosław Bakacz <jaroslaw.bakacz@tuwcuprum.local>
2024-08-26 08:11:11 +0200

    Zadanie domowe 2

---
+++ homework_2.py
@@ -0,0 +1,15 @@
+import datetime as d
+
+
+print("Witamy w generatorze życzeń urodzinowych!")
+imie_nad = input("Podaj swoje imię: ")
+imie_odb = input("Podaj imię solenizanta: ")
+rok_ur = int(input("Podaj rok urodzenia solenizanta: "))
+personal = input("Co chciałbyś dodać do życzeń od siebie?: ")
+
+rok_akt = d.date.today().year
+liczba_lat = rok_akt - rok_ur
+
+print("Poniżej Twoje życzenia: \n")
+print(f"{imie_odb} \nWszystkiego najlepszego z okazji {liczba_lat} urodzin. \n{personal} \n{imie_nad}")
+
