2031b45fcf096679c0336eeb10783892ade28ac8
JarekBak <j.bakacz76@gmail.com>
2024-08-27 12:02:39 +0200

    Robi to samo co wcześniej, ale z pętlą for

--- homework3.py
+++ homework3.py
@@ -1,24 +1,17 @@
 print("--- PROGRAM DO OBSŁUGI WYSYŁKI PACZEK ---")
 
 liczba_el = int(input("Podaj liczbę elementów do wysłania: "))
-
-i=0
+i = 0
 waga_paczki = 0
-while i < liczba_el:
-    waga_el = float(input(f"Podaj wagę elementu nr {i+1} do wysłania: ")) #podajemy dane o elementach do spakowania
-#
-    if waga_el <1 or waga_el >10: #warunek, dla którego nie można wysłać elementu
-        print("Zła waga elementu - nie nadaje się do wysłania!")
 
-    else: #sumuje wagę dopuszczonych do wysyłki elementów
+for paczki in range(liczba_el):
+    # podajemy dane o elementach do spakowania
+    waga_el = float(input(f"Podaj wagę elementu nr {i+1} do wysłania: "))
+    i += 1
+    # warunek, dla którego nie można wysłać elementu
+    if waga_el < 1 or waga_el > 10:
+        print("Zła waga elementu - nie nadaje się do wysłania!")
+    else:
         waga_paczki = waga_paczki + waga_el
-        #ile_wysylka = liczba_el - ile
-        i += 1
-#     for waga_el in range (liczba_el):
-#         if waga_el <1 or waga_el >10: #warunek, dla którego nie można wysłać elementu
-#             print("Zła waga elementu - nie nadaje się do wysłania!")
-#         else:
-#             waga_paczki = waga_paczki + waga_el
-#         i += 1
 
 print(f"\n--- PODSUMOWANIE --- \n Liczba elementów w paczce: {i} \n Waga paczki: {waga_paczki} kg")
