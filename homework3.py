c288219a8935a65ca0ca9f03b877903d48047878
JarekBak <j.bakacz76@gmail.com>
2024-08-27 12:20:46 +0200

    Kod zlicza elementy dopuszczone do dodania do paczki

--- homework3.py
+++ homework3.py
@@ -7,11 +7,11 @@ waga_paczki = 0
 for paczki in range(liczba_el):
     # podajemy dane o elementach do spakowania
     waga_el = float(input(f"Podaj wagę elementu nr {i+1} do wysłania: "))
-    i += 1
     # warunek, dla którego nie można wysłać elementu
     if waga_el < 1 or waga_el > 10:
         print("Zła waga elementu - nie nadaje się do wysłania!")
     else:
         waga_paczki = waga_paczki + waga_el
+        i += 1
 
 print(f"\n--- PODSUMOWANIE --- \n Liczba elementów w paczce: {i} \n Waga paczki: {waga_paczki} kg")
