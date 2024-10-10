9ff127a7a24165171ba58aec203b855be8cb76ee
JarekBak <j.bakacz76@gmail.com>
2024-10-10 17:57:07 +0200

    Finalna wersja programu po wprowadzeniu poprawek

--- homework3.py
+++ homework3.py
@@ -33,24 +33,27 @@ for paczki in range(liczba_el):
         if nowa_waga_paczki > max_paczki:
             nowa_waga_paczki = akt_waga_paczki
             #dodanie kolejnej paczki
+            print("Paczka zamknięta")
+            if puste_miejsce > max_puste_miejsce:
+                max_puste_miejsce = puste_miejsce
+                nr_paczki_puste_miejsce = l_paczek
             l_paczek += 1
-
         # sumaryczna waga wysłanych elementów
         waga_sum += akt_waga_paczki
 
         # sprawdzenie pustego miejsca w paczce
         puste_miejsce = max_paczki - nowa_waga_paczki
 
-        # weryfikacja, w której paczce jest najwięcej pustego miejsca
-        if puste_miejsce > max_puste_miejsce:
-            max_puste_miejsce = puste_miejsce
-            nr_paczki_puste_miejsce = l_paczek
-
         print(f"Waga paczki {l_paczek}: {nowa_waga_paczki}")
         print(f"W paczce zostało {puste_miejsce} kg")
 
+# sprawdzenie ostatniej paczki odnośnie pustego miejsca + weryfikacja czy nie jest ono największe
+if puste_miejsce > max_puste_miejsce:
+    max_puste_miejsce = puste_miejsce
+    nr_paczki_puste_miejsce = l_paczek
+
 print("\n" + "-" * 5 + "PODSUMOWANIE" + "-" * 5)
 print(f"Liczba wysłanych paczek: {l_paczek}")
-print(f"W sumie wysłano: {waga_sum} kg")
+print(f"Sumaryczna waga wszystkich paczek: {waga_sum} kg")
 print(f"Najwięcej pustego miejsca jest w paczce nr {nr_paczki_puste_miejsce}")
-print(f"Zostało w niej {puste_miejsce} kg")
+print(f"Zostało w niej {max_puste_miejsce} kg")
